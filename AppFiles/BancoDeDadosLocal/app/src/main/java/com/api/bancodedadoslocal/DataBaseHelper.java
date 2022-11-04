65862dbed734dfedcf084adabd94ec5940b6e768
lucas.martello <lucasmartello1@gmail.com>
2022-11-04 20:28:11 -0300

    save

---
+++ AppFiles/BancoDeDadosLocal/app/src/main/java/com/api/bancodedadoslocal/DataBaseHelper.java
@@ -0,0 +1,24 @@
+package com.api.bancodedadoslocal;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public DataBaseHelper(@Nullable Context context,) {
+        super(context, "lista.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
